58f15bcd20e9d0047557d09e9846089fb1577db1
sc <barnawibusiness@gmail.com>
2024-01-21 00:53:27 -0800

    Initial commit

---
+++ devel/share/vesc_msgs/cmake/vesc_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(vesc_msgs_MESSAGE_FILES "/home/scsc/f1tenth_ws/src/f1tenth_system/vesc/vesc_msgs/msg/VescState.msg;/home/scsc/f1tenth_ws/src/f1tenth_system/vesc/vesc_msgs/msg/VescStateStamped.msg")
+set(vesc_msgs_SERVICE_FILES "")
